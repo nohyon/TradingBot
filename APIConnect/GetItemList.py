aef46b73fd590b0b1c0ee5b42d5138de64152438
Noyeon Park <nohyon@icloud.com>
2019-09-16 13:28:25 +0900

    db부분

--- APIConnect/GetItemList.py
+++ APIConnect/GetItemList.py
@@ -32,32 +32,39 @@ def WriteKosdaq():
     connect.close
 '''
 
+#받은 데이터를 테이블차트로 변환
+def DataToTableChart(ItemCode):
 # 차트 객체 구하기
+    inStockChart.SetInputValue(0, ItemCode)   #종목 코드
+    inStockChart.SetInputValue(1, ord('2')) # 개수로 조회
+    inStockChart.SetInputValue(4, 20) # 최근 20일 치
+    inStockChart.SetInputValue(5, [0,2,3,4,5,8]) #날짜,시가,고가,저가,종가,거래량
+    inStockChart.SetInputValue(6, ord('D')) # '차트 주가 - 일간 차트 요청
+    inStockChart.SetInputValue(9, ord('1')) # 수정주가 사용
+    inStockChart.BlockRequest()
+    
+    len = inStockChart.GetHeaderValue(3)
+    for i in range(len):
+    Date = inStockChart.GetDataValue(0, i)
+    Open = inStockChart.GetDataValue(1, i)
+    High = inStockChart.GetDataValue(2, i)
+    Low = inStockChart.GetDataValue(3, i)
+    Closing = inStockChart.GetDataValue(4, i)
+    Volumn = inStockChart.GetDataValue(5, i)
+    print (day, open, high, low, close, vol)
+    
+    ItemTableChart = DataFrame(item columns=[])
 
- 
-inStockChart.SetInputValue(0, ItemCode)   #종목 코드
-inStockChart.SetInputValue(1, ord('2')) # 개수로 조회
-inStockChart.SetInputValue(4, 20) # 최근 20일 치
-inStockChart.SetInputValue(5, [0,2,3,4,5,8]) #날짜,시가,고가,저가,종가,거래량
-inStockChart.SetInputValue(6, ord('D')) # '차트 주가 - 일간 차트 요청
-inStockChart.SetInputValue(9, ord('1')) # 수정주가 사용
-inStockChart.BlockRequest()
-
-len = inStockChart.GetHeaderValue(3)
 
 #KOSPI.db의 테이블에 정보를 입력하는 부분입니다.
 def StoreKospiData():
     connect = sqlite3.connect("KOSPI.db")
+    CODE = #get table name 
+    
+    DataToTableChart(CODE)
+    #
     cursor.execute("INSERT INTO "+code+" VALUES(Date text, Open int, High int, Low int, Closing int, Volumn int)")
-    for i in range(len):
-    day = inStockChart.GetDataValue(0, i)
-    open = inStockChart.GetDataValue(1, i)
-    high = inStockChart.GetDataValue(2, i)
-    low = inStockChart.GetDataValue(3, i)
-    close = inStockChart.GetDataValue(4, i)
-    vol = inStockChart.GetDataValue(5, i)
-    prt (day, open, high, low, close, vol)
-
+    
 
 
 
