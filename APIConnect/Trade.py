7d0b796f4264b90e6ad884a90518a28ae096e4b5
Noyeon Park <nohyon@icloud.com>
2019-12-26 23:33:08 +0900

    asdasdasd

--- APIConnect/Trade.py
+++ APIConnect/Trade.py
@@ -1,35 +1,30 @@
 import win32com.client
 
+CpTdUtil = win32com.client.Dispatch("CpTrade.CpTdUtil")
+CpTd0311 = win32com.client.Dispatch("CpTrade.CpTd0311")
+
 #주문초기화
-Trade =  win32com.client.Dispatch("CpTrade.CpTdUtil")
 initCheck = objTrade.TradeInit(0)
 if (initCheck != 0):
     print("주문 초기화 실패")
     exit()
 
-def GetItem():
-    from ChatBot import ItemOrder
-    
+#주식매매주문
+Tradetype = 
+Account = objTrade.AccountNumber[0] #계좌번호
+Itemcode = 
+Quantity =
+Price = 
 
-#주식매수주문
-acc = objTrade.AccountNumber[0] #계좌번호
-accFlag = objTrade.GoodsList(acc, 1)  # 주식상품 구분
-print(acc, accFlag[0])
-objStockOrder = win32com.client.Dispatch("CpTrade.CpTd0311")
-objStockOrder.SetInputValue(0, "2")   # 2: 매수
-objStockOrder.SetInputValue(1, acc )   #  계좌번호
-objStockOrder.SetInputValue(2, accFlag[0])   # 상품구분 - 주식 상품 중 첫번째
-objStockOrder.SetInputValue(3, "A003540")   # 종목코드 - A003540 - 대신증권 종목
-objStockOrder.SetInputValue(4, 10)   # 매수수량 10주
-objStockOrder.SetInputValue(5, 14100)   # 주문단가  - 14,100원
-objStockOrder.SetInputValue(7, "0")   # 주문 조건 구분 코드, 0: 기본 1: IOC 2:FOK
-objStockOrder.SetInputValue(8, "01")   # 주문호가 구분코드 - 01: 보통
+CpTd0311.SetInputValue(0, Tradetype)   # 1:매도, 2:매수
+CpTd0311.SetInputValue(1, Account)   #  계좌번호
+CpTd0311.SetInputValue(3, Itemcode)   # 종목코드
+CpTd0311.SetInputValue(4, Quantity)   # 수량
+CpTd0311.SetInputValue(5, Price)   # 주문단가
+CpTd0311.BlockRequest() # 주문 전송
  
-# 매수 주문 요청
-objStockOrder.BlockRequest()
- 
-rqStatus = objStockOrder.GetDibStatus()
-rqRet = objStockOrder.GetDibMsg1()
+rqStatus = CpTd0311.GetDibStatus()
+rqRet = CpTd0311.GetDibMsg1()
 print("통신상태", rqStatus, rqRet)
 if rqStatus != 0:
     exit()
